f54dca76b9fbc79c19bcb039e2c7c6e2f37e41f8
JoaKam <joannakamienik@wp.pl>
2017-10-29 18:33:13 +0100

    Exercise 15.3

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+    private Long id;
 
     @Column(name = "name")
     private String title;
